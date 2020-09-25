5c081bfcd5ab4f5e8306e82ffd55635028195b3b
bulss <bulssajo00@gmail.com>
2020-09-25 19:24:36 +0900

    HackerRank solve

---
+++ src/com/company/test/success/Pro_Rank_0925_01.java
@@ -0,0 +1,65 @@
+package com.company.test.success;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Pro_Rank_0925_01 {
+    //Queues and Stacks
+    //같은 단어를 큐 와 스택에 넣음 으로써
+    //stack에 입력한 단어는 좌우가 반전이 된다.
+    //나온 단어들이 같으면 회문이 됨을 알 수 있다
+
+    Stack<Character> stack = new Stack<>();
+    Queue<Character> queue = new LinkedList<>();
+
+    void pushCharacter(char ch) {
+        stack.push(ch);
+    }
+
+    void enqueueCharacter(char ch) {
+        queue.add(ch);
+    }
+
+    char popCharacter() {
+        return stack.pop();
+    }
+
+    char dequeueCharacter() {
+        return queue.remove();
+    }
+
+
+    public static int solution() {
+        Scanner scan = new Scanner(System.in);
+        String input = scan.nextLine();
+        scan.close();
+
+        // Convert input String to an array of characters:
+        char[] s = input.toCharArray();
+
+        // Create a Solution object:
+        Pro_Rank_0925_01 p = new Pro_Rank_0925_01();
+
+        // Enqueue/Push all chars to their respective data structures:
+        for (char c : s) {
+            p.pushCharacter(c);
+            p.enqueueCharacter(c);
+        }
+
+        // Pop/Dequeue the chars at the head of both data structures and compare them:
+        boolean isPalindrome = true;
+        for (int i = 0; i < s.length/2; i++) {
+            if (p.popCharacter() != p.dequeueCharacter()) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        //Finally, print whether string s is palindrome or not.
+        System.out.println( "The word, " + input + ", is "
+                + ( (!isPalindrome) ? "not a palindrome." : "a palindrome." ) );
+    }
+
+}
